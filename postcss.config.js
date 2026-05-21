65337ac6a0b68a0122eca87fae067f52ebca718d
Ryan <ryanrestian34@gmail.com>
2026-05-22 06:18:43 +0700

    fix tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
-};
+  plugins: [],
+}
